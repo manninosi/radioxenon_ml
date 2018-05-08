Fixed variance and test_functions to work as intended
Also added two import lines in test_functions to allow for refreshing of variance and ml_matrix_composition files after reloading (lines 12-13). Before, if I changed variance and reran test_variance() it would use the old version. With this added in, it uses the new version. I'm currently not sure why this works this way

radioxenon_ml/solve/variance.py
@@ -6,7 +6,6 @@ Created on Mon May  7 22:15:54 2018
 """
 import numpy as np
 
-
 def variance(q, AS, f):
     """
     Determines the variance of the number of counts in each channel 
@@ -15,18 +14,25 @@ def variance(q, AS, f):
         -AS is either the estimated activity of the kth nuclide A or 
             the experimental sample spectrum S (if first iteration)
         -f is the reference spectrum
+    
+    Equation is taken from
     """
+    D_temp = np.zeros((len(AS),len(f)))
+    D = np.zeros(len(f))
+    
     if q < 0:
         print("Iteration must be greater than 0! Exiting...")
         return 
-    else:
+    
+    else:      
         if q > 0:
-            D = np.sum(AS*f)
+            for k in range(len(AS)):        #loop over # of isotopes
+                for i in range(len(f)):         #loop over # of array elements
+                    D_temp[k,i] = AS[k]*f[i]   #Eqn. 5
+            
+            D = np.sum(D_temp,axis=0)  
+            
         else:
-            if len(AS) != len(f):
-                print("Please make your two arrays of equal length! Exiting...")
-                return 
-            else:
-                D = AS+1
+            D = AS+1
         
         return D

radioxenon_ml/test_files/test_functions.py
@@ -7,6 +7,10 @@ from radioxenon_ml.read_in import ml_matrix_composition as mlmc
 from radioxenon_ml.solve import variance as v
 import numpy as np
 
+#for some reason the following import must occur in order to refresh any changes to variance 
+#if I try to import it earlier it does not work
+import radioxenon_ml.solve.variance
+import radioxenon_ml.read_in.ml_matrix_composition
 
 def test_file_existence():
     """
@@ -94,14 +98,19 @@ def test_variance():
     test the variance function using two known vectors
     """
     S = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
-    A = np.array([1,1,1,1,2,2,2,2,3,3,3,3])
+    A = np.array([1,2,3,4])
     f = np.array([0,0,0,0,0,0,1,1,1,1,1,1])
     D=np.array([])   
     for q in range(0,3):
         if q == 0:
-            D=np.append(D,v.variance(q,S,f))
+            D=v.variance(q,S,f)
+            assert len(D)==len(S)
+            assert D[1] == S[2]
+            print("\nFirst iteration is correct!")
         else:
-            D=np.append(D,v.variance(q,A,f))
-        print(D)
+            D=v.variance(q,A,f)
+            assert len(D) == len(f)
+            print("\nLengths are proper!")
+       return
     
         